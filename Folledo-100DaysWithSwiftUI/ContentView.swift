e10bc88228fdc9ff6796a90959c245a4263ab725
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-25 22:47:29 -0700

    Initial Commit

---
+++ Folledo-100DaysWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Folledo-100DaysWithSwiftUI
+//
+//  Created by Macbook Pro 15 on 9/25/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Folledo-100DaysWithSwiftUITests/Folledo_100DaysWithSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Folledo_100DaysWithSwiftUITests.swift
+//  Folledo-100DaysWithSwiftUITests
+//
+//  Created by Macbook Pro 15 on 9/25/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import XCTest
+@testable import Folledo_100DaysWithSwiftUI
+
+class Folledo_100DaysWithSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
